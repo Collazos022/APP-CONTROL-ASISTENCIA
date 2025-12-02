d1d8af01f64c4efd128b833779c2929972ba02f4
Carlos Collazos Doncel <cared022@gmail.com>
2025-12-02 18:19:02 +0000

    Try fixing this error: `Console Error: `DialogContent` requires a `Dialo

--- src/components/dashboard-header.tsx
+++ src/components/dashboard-header.tsx
@@ -4,6 +4,8 @@ import Link from 'next/link';
 import {
   Sheet,
   SheetContent,
+  SheetHeader,
+  SheetTitle,
   SheetTrigger,
 } from '@/components/ui/sheet';
 import { Button } from '@/components/ui/button';
@@ -74,6 +76,9 @@ export function DashboardHeader() {
           </Button>
         </SheetTrigger>
         <SheetContent side="left" className="sm:max-w-xs">
+           <SheetHeader>
+              <SheetTitle className="sr-only">Menú de Navegación</SheetTitle>
+            </SheetHeader>
           <nav className="grid gap-6 text-lg font-medium">
             <Link
               href="#"
